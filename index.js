dc3a3b83b6194f7dbe2d4cd06e464333f8928d96
Mar Kal <marc@example.com>
2023-02-08 20:47:58 +0100

    feat: finish 3.2 task

--- index.js
+++ index.js
@@ -34,6 +34,16 @@ app.get("/api/persons", (request, response) => {
   response.json(persons);
 });
 
+app.get("/info", (request, response) => {
+  const date = new Date();
+  response.send(
+    `
+    <div>Phonebook has info for ${persons.length} people</div>
+    <p>${date}</p>
+    `
+  );
+});
+
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
