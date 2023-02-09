514fa4904299a72c3243b9ad496602d6b6dbcab5
Mar Kal <marc@example.com>
2023-02-09 18:09:24 +0100

    feat: finish 3.7 task

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require("express");
+const morgan = require("morgan");
+
 const app = express();
 
 app.use(express.json());
+
+// create morgan middleware
+app.use(morgan("tiny"));
 
 const API_URL = "/api/persons";
 
@@ -98,6 +103,12 @@ app.post(API_URL, (request, response) => {
 
   response.json(person);
 });
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
+
+app.use(unknownEndpoint);
 
 const PORT = 3001;
 app.listen(PORT, () => {
